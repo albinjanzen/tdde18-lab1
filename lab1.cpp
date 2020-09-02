b3f8623a900d9c756e81b87808f7f6cf08421acd
albinjanzen <albinjanzen@gmail.com>
2020-09-02 21:27:56 +0200

    lab 1 del 1 klar, behöver provas

--- lab1.cpp
+++ lab1.cpp
@@ -8,72 +8,80 @@ int number{};
 float real{};
 char character{};
 string str{};
-/*
+
 cout << "Enter one integer: " << flush;
 cin >> number;
-cout << "You entered the number: " << number << endl;
-cout << endl;
+getline(cin, str);
+cout << "You entered the number: " << number << endl << endl;
 
 cout << "Enter four integers: ";
 cin >> number;
 cout << "You entered the numbers: " << number << " ";
-cout << number << " ";
 cin >> number;
 cout << number << " ";
 cin >> number;
-cout << number << endl;
-cout << endl;
+cout << number << " ";
+cin >> number;
+cout << number << endl << endl;
+getline(cin, str);
 
 cout << "Enter one integer and one real number: ";
 cin >> number;
 cin >> real;
 cout << "The real is: " << setw(11) << setprecision(4) << real << endl;
-cout << "the integer is: " << setw(8) << number << endl;
-cout << endl;
+cout << "the integer is: " << setw(8) << number << endl << endl;
+getline(cin, str);
 
-cout << "Enter one integer and one real number: ";
+cout << "Enter one real and one integer number: ";
 cin >> real;
 cin >> number;
-cout << "The real is: " << setfill('.') << setw(11) << setprecision(4) << real << endl;
-cout << "the integer is: " << setfill('.') << setw(8) << number << endl;
-cout << endl;
+cout << "The real is: " << setfill('.') << setw(11) << fixed << setprecision(3) << real << endl;
+cout << "the integer is: " << setfill('.') << setw(8) << number << endl << endl;
+getline(cin, str);
 
 cout << "Enter a character: ";
 cin >> character;
-cout << "You entered: " << character << endl;
-cout << endl;
+cout << "You entered: " << character << endl << endl;
+getline(cin, str);
 
 cout << "Enter a word: ";
 cin >> str;
-cout << "The word '" << str << "' has " << str.size() << " character(s)" << endl;
-cout << endl; 
+cout << "The word '" << str << "' has " << str.size() << " character(s)" << endl << endl;
+getline(cin, str);
 
 cout << "Enter an integer and a word: ";
 cin >> number;
 cin >> str;
-cout << "You entered '" << number << "' and '" << str <<"'." << endl;
-cout << endl;
+cout << "You entered '" << number << "' and '" << str <<"'." << endl << endl;
+getline(cin, str);
 
 cout << "Enter a character and a word: ";
 cin >> character;
 cin >> str;
-cout << "You entered the string \""<< str << "\" and the character ' << character << "'." << endl;
-cout << endl;
+cout << "You entered the string \""<< str << "\" and the character '" << character << "'." << endl << endl;
+getline(cin, str);
 
 cout << "Enter a word and a real number: ";
 cin >> str;
 cin >> real;
-cout << "You entered: \"" << str << "\" and \"" << fixed << setprecision(3) << real << "\"." << endl;
-cout << endl;
+cout << "You entered: \"" << str << "\" and \"" << fixed << setprecision(3) << real << "\"." << endl << endl;
+getline(cin, str);
 
 cout << "Enter a text-line: ";
 getline(cin, str);
-cout << "You entered: \"" << str << "\"";
-cout << endl;
-*/
+cout << "You entered: \"" << str << "\"" << endl << endl;
 
 cout << "Enter a second line of text: ";
 getline(cin, str);
-cout << "You entered: '" << str << "\"";
+cout << "You entered: '" << str << "\"" << endl << endl;
+
+cout << "Enter three words: ";
+cin >> str;
+cout << "You entered: '" << str << " ";
+cin >> str;
+cout << str << " ";
+cin >> str;
+cout << str << "'" << endl << endl;
+
 return 0;
 }
