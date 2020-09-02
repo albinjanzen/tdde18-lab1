0a6a992ca7337278b2e265584fdc33bed4e03448
albinjanzen <albinjanzen@gmail.com>
2020-09-02 15:13:40 +0200

    test2

--- lab1.cpp
+++ lab1.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Enter one integer" << flush;
+int numberOne{};
+cin >> numberOne;
+cout << "You entered the number: " << numberOne << endl;
+cout << endl;
+return 0;
+}
