4969e03c85e974d6197fb093d7ed62044962635f
albinjanzen <albinjanzen@gmail.com>
2020-09-02 17:29:44 +0200

    three

--- lab1.cpp
+++ lab1.cpp
@@ -1,12 +1,79 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-cout << "Enter one integer" << flush;
-int numberOne{};
-cin >> numberOne;
-cout << "You entered the number: " << numberOne << endl;
+int number{};
+float real{};
+char character{};
+string str{};
+/*
+cout << "Enter one integer: " << flush;
+cin >> number;
+cout << "You entered the number: " << number << endl;
 cout << endl;
+
+cout << "Enter four integers: ";
+cin >> number;
+cout << "You entered the numbers: " << number << " ";
+cout << number << " ";
+cin >> number;
+cout << number << " ";
+cin >> number;
+cout << number << endl;
+cout << endl;
+
+cout << "Enter one integer and one real number: ";
+cin >> number;
+cin >> real;
+cout << "The real is: " << setw(11) << setprecision(4) << real << endl;
+cout << "the integer is: " << setw(8) << number << endl;
+cout << endl;
+
+cout << "Enter one integer and one real number: ";
+cin >> real;
+cin >> number;
+cout << "The real is: " << setfill('.') << setw(11) << setprecision(4) << real << endl;
+cout << "the integer is: " << setfill('.') << setw(8) << number << endl;
+cout << endl;
+
+cout << "Enter a character: ";
+cin >> character;
+cout << "You entered: " << character << endl;
+cout << endl;
+
+cout << "Enter a word: ";
+cin >> str;
+cout << "The word '" << str << "' has " << str.size() << " character(s)" << endl;
+cout << endl; 
+
+cout << "Enter an integer and a word: ";
+cin >> number;
+cin >> str;
+cout << "You entered '" << number << "' and '" << str <<"'." << endl;
+cout << endl;
+
+cout << "Enter a character and a word: ";
+cin >> character;
+cin >> str;
+cout << "You entered the string \""<< str << "\" and the character ' << character << "'." << endl;
+cout << endl;
+
+cout << "Enter a word and a real number: ";
+cin >> str;
+cin >> real;
+cout << "You entered: \"" << str << "\" and \"" << fixed << setprecision(3) << real << "\"." << endl;
+cout << endl;
+
+cout << "Enter a text-line: ";
+getline(cin, str);
+cout << "You entered: \"" << str << "\"";
+cout << endl;
+*/
+
+cout << "Enter a second line of text: ";
+getline(cin, str);
+cout << "You entered: '" << str << "\"";
 return 0;
 }
