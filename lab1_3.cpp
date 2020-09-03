787e08d0c51f71e778482fcf3514911973567eaf
albinjanzen <albinjanzen@gmail.com>
2020-09-03 16:52:22 +0200

    lab1_3 done

---
+++ lab1_3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream doc{"text.txt"};
+    string s{};
+    string l{};
+    string k{};
+    int size{0}, letters{0};
+    float average{0};
+    doc >> s;
+    ++size;
+    l = s;
+    k = s;
+    while (doc >> s) {
+        ++size;
+        letters = letters + s.size();
+        if (s.size()>l.size()) {
+            l=s;
+        } else if (s.size()<k.size())
+        {
+            k=s;
+        }
+    }
+    if (!(average=0.00)) {
+        average = letters/size;
+        cout << "There are " << size << " words in the file." << endl;
+        cout << "The shortest word was \"" << k << "\" with " << k.size() << " character(s)." << endl;
+        cout << "The longest word was \"" << l << "\" with " << l.size() << " character(s)." << endl;
+        cout << "The average length was " << fixed << setprecision(2) << average << " character(s)." << endl;
+    } else {
+        cout << "Error: file is empty." << endl;
+    }
+    
+}
